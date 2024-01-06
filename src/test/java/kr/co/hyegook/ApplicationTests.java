82b9d3b9ab58263a63a21e07e9af527af45c6c63
hyejin <chjin4520@naver.com>
2024-01-06 20:00:57 +0900

    1

---
+++ src/test/java/kr/co/hyegook/ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.hyegook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
